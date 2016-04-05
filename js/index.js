6c95b48bf21c76c637b587ecab6c8a296f527133
DJYpersonal <1143463819@qq.com>
2016-04-06 00:26:17 +0800

    ，，

--- js/index.js
+++ js/index.js
@@ -1,26 +1,26 @@
 var ban=document.getElementsByClassName("banner")[0];
-      var imgs=document.getElementsByClassName("tab_pannel",ban);
-      var lis=document.getElementsByClassName("span");
-      var btnL=document.getElementsByClassName("btnL")[0];
-      var btnR=document.getElementsByClassName("btnR")[0];
+var imgs=document.getElementsByClassName("tab_pannel",ban);
+var lis=document.getElementsByClassName("span");
+var btnL=document.getElementsByClassName("btnL")[0];
+var btnR=document.getElementsByClassName("btnR")[0];
 
-      var t;
-      var num=0;
-      var flag=true;
-      t=setInterval(moveR,2000);
+var t;
+var num=0;
+var flag=true;
+t=setInterval(moveR,2000);
 
      //鼠标移上停止，移走继续自动轮播
-      ban.onmouseover=function(){
-      	clearInterval(t);
-      	btnL.style.display="block";
-        btnR.style.display="block";
-      }
-      ban.onmouseout=function(){
-        clearInterval(t);
-        t=setInterval(moveR,2000);
-        btnL.style.display="none";
-      	btnR.style.display="none";
-      }
+     ban.onmouseover=function(){
+     	clearInterval(t);
+     	btnL.style.display="block";
+     	btnR.style.display="block";
+     }
+     ban.onmouseout=function(){
+     	clearInterval(t);
+     	t=setInterval(moveR,2000);
+     	btnL.style.display="none";
+     	btnR.style.display="none";
+     }
 
      //圆点按钮
      for(var i=0;i<lis.length;i++){
@@ -29,13 +29,13 @@ var ban=document.getElementsByClassName("banner")[0];
      		num=this.index;
      		for(var j=0;j<imgs.length;j++){
      			animate(imgs[j],{opacity:0});
-      		    lis[j].style.background="#211616"
-      	}
-      	animate(imgs[this.index],{opacity:1});
-      	lis[this.index].style.background="#e5004f";
+     			lis[j].style.background="#211616"
      		}
+     		animate(imgs[this.index],{opacity:1});
+     		lis[this.index].style.background="#e5004f";
      	}
-    
+     }
+
 
 
 
@@ -49,15 +49,15 @@ var ban=document.getElementsByClassName("banner")[0];
      
      function moveL(){
      	num--;
-      	if(num<0){
-      		num=imgs.length-1;
-      	}
-      	for(var i=0;i<imgs.length;i++){
-      		animate(imgs[i],{opacity:0});
-      		lis[i].style.background="#211616";
-      	}
-      	animate(imgs[num],{opacity:1});
-      	lis[num].style.background="#e5004f";
+     	if(num<0){
+     		num=imgs.length-1;
+     	}
+     	for(var i=0;i<imgs.length;i++){
+     		animate(imgs[i],{opacity:0});
+     		lis[i].style.background="#211616";
+     	}
+     	animate(imgs[num],{opacity:1});
+     	lis[num].style.background="#e5004f";
      }
      
       //自动轮播
@@ -74,3 +74,23 @@ var ban=document.getElementsByClassName("banner")[0];
       	lis[num].style.background="#e5004f";
 
       }
+
+var car=document.getElementsByClassName('minishop')[0];
+var item=document.getElementsByClassName('shop-item')[0];
+car.onmouseover=function(){
+    item.style.display="block";
+}
+car.onmouseout=function(){
+	item.style.display="none";
+}
+
+var weixin=document.getElementsByClassName('log-wechat')[0];
+var wechat=document.getElementsByClassName('wechat')[0];
+weixin.onmouseover=function(){
+	weixin.classList.add('yt-wechat');
+	wechat.style.display="block";
+}
+weixin.onmouseout=function(){
+	weixin.classList.remove('yt-wechat');
+	wechat.style.display="none";
+}
